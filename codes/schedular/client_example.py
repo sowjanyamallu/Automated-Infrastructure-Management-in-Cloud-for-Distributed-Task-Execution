cb0fe7e2a6671f8cb50237bba3c966a6111a2976
sowjanyamallu <smallu1@hawk.iit.edu>
2018-06-27 01:06:31 -0500

    Update client_example.py

--- codes/schedular/client_example.py
+++ codes/schedular/client_example.py
@@ -5,5 +5,5 @@ channel = connection.channel()
 channel.queue_declare(queue='tasks')
 channel.basic_publish(exchange='',
                       routing_key='tasks',
-                      body='convert https://storage.googleapis.com/vnagpure-videos/Hotel%20California.mp4')
+                      body='convert https://storage.googleapis.com/smallu-videos/Hotel%20California.mp4')
 print(" [x] Sent 'Hello World!'")
